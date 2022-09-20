b85f0c8ef7bd3e40c0aa9b61cab76d9ce2960af1
Ruan Noleto <84647446+ruannysil@users.noreply.github.com>
2022-09-20 11:06:28 -0300

    update project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import './style.css'
+// https://sujeitoprogramador.com/rn-api/?api=posts
 
 function App() {
+  const[nutri, setNutri] = useState([]);
+
+  useEffect(()=> {
+    function loadApi() {
+      let url = 'https://sujeitoprogramador.com/rn-api/?api=posts';
+      fetch(url)
+      .then((r) => r.json())
+      .then((json) =>{
+        setNutri(json);
+      })
+    }
+
+    loadApi();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='container'>
+      <header>
+        <strong>React Nutri</strong>
       </header>
+      {nutri.map((item)=> {
+        return(
+          <article key={item.id} className="post">
+            <strong className='titulo'>{item.titulo}</strong>
+            <h3 className='categoria'>categoria: {item.categoria}</h3>
+            <img src={item.capa} alt={item.titulo} className='capa' />
+            <p className='subtitulo'>
+              {item.subtitulo}
+            </p>
+            <span className='botao'>Acessar</span>
+          </article>
+        )
+      })}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
